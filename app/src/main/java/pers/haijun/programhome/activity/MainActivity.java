67f6b0cdd825dbcb965fed804c347ae973eaad75
jianghaijun <aini1320666709@foxmail.com>
2017-10-31 14:17:15 +0800

    Initial commit

---
+++ app/src/main/java/pers/haijun/programhome/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package pers.haijun.programhome.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pers.haijun.programhome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
