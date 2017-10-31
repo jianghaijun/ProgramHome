b400822470d0cda8fad135f61369a02de34f844f
jianghaijun <aini1320666709@foxmail.com>
2017-10-31 16:06:25 +0800

    集成Mob短信验证码功能

--- app/src/main/java/pers/haijun/programhome/activity/MainActivity.java
+++ app/src/main/java/pers/haijun/programhome/activity/MainActivity.java
@@ -2,14 +2,53 @@ package pers.haijun.programhome.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
 import pers.haijun.programhome.R;
 
 public class MainActivity extends AppCompatActivity {
+    private EventHandler eventHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 如果希望在读取通信录的时候提示用户，可以添加下面的代码，并且必须在其他代码调用之前，否则不起作用；如果没这个需求，可以不加这行代码
+        SMSSDK.setAskPermisionOnReadContact(true);
+
+        // 创建EventHandler对象
+        eventHandler = new EventHandler() {
+            public void afterEvent(int event, int result, Object data) {
+                if (data instanceof Throwable) {
+                    Throwable throwable = (Throwable)data;
+                    String msg = throwable.getMessage();
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
+                        // 处理你自己的逻辑
+                    }
+                }
+            }
+        };
+
+        // 注册监听器
+        SMSSDK.registerEventHandler(eventHandler);
+
+        findViewById(R.id.txtSendSms).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取验证码
+                SMSSDK.getVerificationCode("86","13504255657");
+            }
+        });
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        SMSSDK.unregisterEventHandler(eventHandler);
     }
 }
